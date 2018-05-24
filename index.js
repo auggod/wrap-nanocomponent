acf7b0d9a7807463d7fdb5b32992b276119e10bf
auggod <hello@auggod.tech>
2018-05-24 13:57:55 +0200

    Init

---
+++ index.js
@@ -0,0 +1,8 @@
+const hasWindow = typeof window !== 'undefined'
+
+module.exports = function (Component) {
+  return function (name, state, emit) {
+    if (hasWindow) return state.cache(Component, name)
+    return Component(name, state, emit)
+  }
+}
